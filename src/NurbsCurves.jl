10b41f58662bedb5251bdaac24f4a46da886a60c
marinlauber <marinlauber@tudelft.nl>
2024-03-08 17:10:03 +0100

    add shift in plot recipt

--- src/NurbsCurves.jl
+++ src/NurbsCurves.jl
@@ -140,21 +140,21 @@ end
 
 Plot `recipe`` for `NurbsCurve``, plot the `NurbsCurve` and the control points.
 """
-@recipe function f(C::NurbsCurve, N::Integer=100; add_cp=true, shifts=[0.,0.])
+@recipe function f(C::NurbsCurve, N::Integer=100; add_cp=true, shift=[0.,0.])
     seriestype := :path
     primary := false
     @series begin
         linecolor := :black
         linewidth := 2
         markershape := :none
-        c = [C(s,0.0).+1.5 for s ∈ 0:1/N:1]
-        getindex.(c,1).+shifts[1],getindex.(c,2).+shifts[2]
+        c = [C(s,0.0) for s ∈ 0:1/N:1]
+        getindex.(c,1).+shift[1],getindex.(c,2).+shift[2]
     end
     @series begin
         linewidth  --> (add_cp ? 1 : 0)
         markershape --> (add_cp ? :circle : :none)
         markersize --> (add_cp ? 4 : 0)
         delete!(plotattributes, :add_cp)
-        C.pnts[1,:].+shifts[1],C.pnts[2,:].+shifts[2]
+        C.pnts[1,:].+shift[1],C.pnts[2,:].+shift[2]
     end
 end
