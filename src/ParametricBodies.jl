ea522dad28ff3915283acce6220e4403758c2930
weymouth <gabriel.weymouth>
2024-12-24 15:18:02 -0700

    Fix extrude
    This PR enables extruded geometry mappings on GPUs.
    1. The `fastd²` version of curve_props now returns the correct length normal and velocity zero vectors.
    2. The `\` operator now uses a special non-allocating method for `a=SMatrix{2,3}`. I added a test set for this will put together a PR for StaticArrays.

--- src/ParametricBodies.jl
+++ src/ParametricBodies.jl
@@ -1,6 +1,29 @@
 module ParametricBodies
 
 using StaticArrays,ForwardDiff
+# Non-allocating method for 2x3 SMatrix solve
+using LinearAlgebra
+import Base: \
+@inline function (\)(a::SMatrix{2,3}, b::SVector{2})
+    # columns of a'
+    a1,a2 = a[1,:],a[2,:]
+    
+    # Q,R decomposition
+    r11 = norm(a1)
+    q1 = a1/r11
+    r12 = q1'*a2
+    p = a2-r12*q1
+    r22 = norm(p) < eps(r11) ? one(r11) : norm(p)
+    q2 = p/r22
+
+    # forward substitution to solve v = R'\b
+    v1 = b[1]/r11
+    v2 = (b[2]-r12*v1)/r22
+
+    # return solution x = Qv 
+    return q1*v1+q2*v2
+end
+
 import WaterLily: AbstractBody,measure,sdf,interp
 
 abstract type AbstractParametricBody <: AbstractBody end
@@ -78,7 +101,7 @@ function curve_props(body::ParametricBody,x,t;fastd²=Inf)
     ξ = body.map(x,t)
     if isfinite(fastd²) && applicable(body.locate,ξ,t,true)
         d = body.scale*body.locate(ξ,t,true)-body.half_thk
-        d^2>fastd² && return d,zero(x),zero(x)
+        d^2>fastd² && return d,zero(ξ),zero(ξ)
     end
 
     # Locate nearest u, and get vector
